9a7e55ad9ac87ecb5e111eadbdbc5ea3ec514605
mhaouel oumaima <o.mhaouel@perenitysoftware.com>
2026-02-12 15:46:42 +0100

    first commit

---
+++ admin-server/src/main/java/org/example/adminserver/authenticatedController.java
@@ -0,0 +1,18 @@
+package org.example.adminserver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class authenticatedController {
+
+    @GetMapping
+    public ResponseEntity<String>  getUser() {
+        return  ResponseEntity.ok("Hello World");
+    }
+}
---
+++ gateway-server/src/test/java/org/example/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,15 @@
+package org.example.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootTest
+@EnableDiscoveryClient
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
